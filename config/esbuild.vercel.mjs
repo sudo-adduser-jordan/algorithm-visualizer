1a7afab219ce0bef113245d18a56d2ce620bff75
jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-08-07 12:18:29 -0700

    Feature: Vercel Build Config
    -add vercel build config

--- config/esbuild.vercel.mjs
+++ config/esbuild.vercel.mjs
@@ -1,3 +1,5 @@
+// Vercel Deploy Build Config
+
 import * as esbuild from "esbuild";
 import fs from "node:fs";
 
@@ -15,21 +17,21 @@ let result = await esbuild.build({
   minify: true,
   // target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
   metafile: true,
-  outdir: "./toolchain/build/dist/",
+  outdir: "./public",
   logLevel: "info",
 });
 
 // write meta file - this can be used at https://esbuild.github.io/analyze/
-fs.writeFileSync(
-  "./toolchain/build/meta.json",
-  JSON.stringify(result.metafile)
-);
+fs.writeFileSync("./public/meta.json", JSON.stringify(result.metafile));
 
 /* print meta file results */
-// console.log(result)
+
+// console.log(result);
 
 console.log(await esbuild.analyzeMetafile(result.metafile));
 
-// console.log(await esbuild.analyzeMetafile(result.metafile, {
+// console.log(
+//   await esbuild.analyzeMetafile(result.metafile, {
 //     verbose: true,
-//   }))
+//   })
+// );
