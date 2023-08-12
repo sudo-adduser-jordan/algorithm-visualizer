571dcd15174f98b0d625bf38b98e0cbb676fd557
jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-08-11 22:35:13 -0700

    Feature: Add Navigation
    -add navigation component
    -add react-icons

---
+++ src/components/navigation.tsx
@@ -0,0 +1,24 @@
+import "./navigation.css";
+import { LuBarChart3 } from "react-icons/lu";
+
+// type NavigationProps = {
+//   data: string[];
+//   title: string;
+//   menu: {
+//     items: string[];
+//   };
+// };
+
+export default function Navigation() {
+  return (
+    <section className="container">
+      <div className="title">
+        <LuBarChart3 className="icon" size={25} />
+        <div>Sorting Algorithms</div>
+      </div>
+      <div className="item">Home</div>
+      <div className="item">Randomize</div>
+      {/* <div>Path Algorithms</div> */}
+    </section>
+  );
+}
--- src/components/sort.tsx
+++ src/components/sort.tsx
@@ -6,6 +6,7 @@ import { shuffleArray } from "../lib/sort/fisher-yates-shuffle";
 import { selectionSort } from "../lib/sort/selection-sort";
 import { quickSort } from "../lib/sort/quick-sort";
 import { bubbleSort } from "../lib/sort/bubble-sort";
+import Navigation from "./navigation";
 // import { mergeSort } from "../lib/sort/merge-sort";
 
 function createArray() {
@@ -50,50 +51,53 @@ export default function Sorting() {
   }
 
   return (
-    <main className="main-container">
-      <section className="bar-container">
-        {array.map((element) => (
-          <motion.div
-            key={element.value}
-            layout
-            className="bar"
-            style={{
-              height: `${element.value + 2}rem`,
-              backgroundColor: element.backgroundColor,
-            }}
-          >
-            {element.value}
-          </motion.div>
-        ))}
-      </section>
-      <button
-        className="button"
-        onClick={() => {
-          sort("Selection");
-        }}
-      >
-        Selection Sort
-      </button>
-      <button
-        className="button"
-        onClick={() => {
-          sort("Bubble");
-        }}
-      >
-        Bubble Sort
-      </button>
-      <button
-        className="button"
-        onClick={() => {
-          sort("Quick");
-        }}
-      >
-        Quick Sort
-      </button>
-      {/* <button onClick={}>Quick Sort</button> */}
-      <button className="button" onClick={() => location.reload()}>
-        Reload
-      </button>
-    </main>
+    <>
+      <Navigation />
+      <main className="main-container">
+        <section className="bar-container">
+          {array.map((element) => (
+            <motion.div
+              key={element.value}
+              layout
+              className="bar"
+              style={{
+                height: `${element.value + 2}rem`,
+                backgroundColor: element.backgroundColor,
+              }}
+            >
+              {element.value}
+            </motion.div>
+          ))}
+        </section>
+        <button
+          className="button"
+          onClick={() => {
+            sort("Selection");
+          }}
+        >
+          Selection Sort
+        </button>
+        <button
+          className="button"
+          onClick={() => {
+            sort("Bubble");
+          }}
+        >
+          Bubble Sort
+        </button>
+        <button
+          className="button"
+          onClick={() => {
+            sort("Quick");
+          }}
+        >
+          Quick Sort
+        </button>
+        {/* <button onClick={}>Quick Sort</button> */}
+        <button className="button" onClick={() => location.reload()}>
+          Reload
+        </button>
+      </main>
+    </>
   );
 }
