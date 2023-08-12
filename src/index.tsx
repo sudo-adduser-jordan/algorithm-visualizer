3e8de4a4a392e29e8b96ad061810c72bc583f7e7
jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-08-12 16:44:20 -0700

    Feature: Add React Strict Mode
    -add react strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
-import ReactDOM from "react-dom/client";
-import App from "./app";
 import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./app";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
+const root = createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
