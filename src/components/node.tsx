135307bc8bad5cbae1542f35238424eee1bf4a2b
jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-08-13 22:11:41 -0700

    Feature: Draw Walls With Mouse
    -add ability to change node to wall with mouse
    -fix eslint errors

--- src/components/node.tsx
+++ src/components/node.tsx
@@ -3,8 +3,28 @@ import "./node.css";
 type NodeProps = {
   row: number;
   column: number;
+  isWall: string;
+  onMouseDown: (row: number, column: number) => void;
+  onMouseEnter: (row: number, column: number) => void;
+  onMouseUp: () => void;
 };
 
-export default function Node({ row, column }: NodeProps) {
-  return <div id={`node-${row}-${column}`} className="node" />;
+export default function Node({
+  row,
+  column,
+  isWall,
+  onMouseDown,
+  onMouseEnter,
+  onMouseUp,
+}: NodeProps) {
+  return (
+    <div
+      id={`node-${row}-${column}`}
+      className={`node ${isWall}`}
+      onMouseDown={() => onMouseDown(row, column)}
+      onMouseEnter={() => onMouseEnter(row, column)}
+      onMouseUp={() => onMouseUp()}
+      role="presentation"
+    />
+  );
 }
--- src/routes/path.tsx
+++ src/routes/path.tsx
@@ -1,40 +1,83 @@
 import "./path.css";
 import { useState } from "react";
 import Node from "../components/node";
+import { motion } from "framer-motion";
 
-function createNode(column: number, row: number) {
-  return {
-    column,
-    row,
-  };
-}
-
-function createGrid() {
-  const grid = [];
-  for (let row = 0; row < 50; row++) {
-    const currentRow = [];
-    for (let column = 0; column < 30; column++) {
-      currentRow.push(createNode(column, row));
-    }
-    grid.push(currentRow);
-  }
-  return grid;
-}
+type Matrix = RowArray[];
+type RowArray = React.JSX.Element[];
 
 export default function Path() {
-  const [grid, setGrid] = useState(createGrid());
+  const [matrix, setMatrix] = useState<Matrix>(createGrid());
+  const [mouseIsPressed, setMousePresed] = useState(false);
+
+  function createGrid() {
+    const matrix: Matrix = [];
+    for (let row = 0; row < 50; row++) {
+      const currentRow: RowArray = [];
+      for (let column = 0; column < 30; column++) {
+        currentRow.push(
+          <Node
+            column={column}
+            row={row}
+            isWall={""}
+            onMouseDown={(row, col) => handleMouseDown(row, col)}
+            onMouseEnter={(row, col) => handleMouseEnter(row, col)}
+            onMouseUp={() => handleMouseUp()}
+          />
+        );
+      }
+      matrix.push(currentRow);
+    }
+    return matrix;
+  }
+
+  function handleMouseDown(row: number, column: number) {
+    const newMatrix = getNewMatrix(matrix, row, column);
+    setMatrix([...newMatrix]);
+    setMousePresed(true);
+  }
+
+  function handleMouseEnter(row: number, column: number) {
+    if (!mouseIsPressed) return;
+    const newMatrix = getNewMatrix(matrix, row, column);
+    setMatrix([...newMatrix]);
+  }
+
+  function handleMouseUp() {
+    setMousePresed(false);
+  }
+
+  function getNewMatrix(matrix: Matrix, row: number, col: number): Matrix {
+    const newMatrix = matrix.slice();
+    if (newMatrix[row][col].props.isWall === "wall") {
+      newMatrix[row][col].props.isWall = "";
+    } else if (newMatrix[row][col].props.isWall === "") {
+      newMatrix[row][col].props.isWall = "wall";
+    }
+    return newMatrix;
+  }
 
   return (
     <main className="path-container">
       <section className="grid-container">
-        {grid.map((row, rowIndex) => {
+        {matrix.map((row, rowIndex) => {
           return (
-            <div key={rowIndex}>
+            <motion.div key={rowIndex}>
               {row.map((node, nodeIndex) => {
-                const { row, column } = node;
-                return <Node key={nodeIndex} column={column} row={row} />;
+                const { row, column, isWall } = node.props;
+                return (
+                  <Node
+                    key={nodeIndex}
+                    row={row}
+                    column={column}
+                    isWall={isWall}
+                    onMouseDown={(row, col) => handleMouseDown(row, col)}
+                    onMouseEnter={(row, col) => handleMouseEnter(row, col)}
+                    onMouseUp={() => handleMouseUp()}
+                  />
+                );
               })}
-            </div>
+            </motion.div>
           );
         })}
       </section>
