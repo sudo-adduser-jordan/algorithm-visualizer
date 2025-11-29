37b5ba7d1f5b72cbb514e997f565c80f453a2a59
jordan <sudo.sendmail.jordan@gmail.com>
2025-11-28 23:29:59 -0800

    remove workflows / update config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    basePath: "/algorithm-visualizer",
+    output: "export",
 };
 
 export default nextConfig;
