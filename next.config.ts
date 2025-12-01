837a4757bf4844210f3e18d23854af0e009a48cc
jordan <sudo.sendmail.jordan@gmail.com>
2025-11-30 20:06:22 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
-    // basePath: "/algorithm-visualizer",
+    basePath: "/algorithm-visualizer",
     output: "export",
 };
 
